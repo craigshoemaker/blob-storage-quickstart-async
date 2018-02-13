19e702195060c2c077ed3466ef743ada53582ce6
= <craig@craigshoemaker.net>
2018-02-13 15:45:35 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const path = require('path');
+const storage = require('azure-storage');
+const args = require('yargs').argv;
+
+const blobService = storage.createBlobService();
+const containerName = 'test';
+const sourceFilePath = path.resolve('./example.txt');
+const blobName = path.basename(sourceFilePath, path.extname(sourceFilePath));
+
+const createContainer = () => {
+    return new Promise((resolve, reject) => {
+        blobService.createContainerIfNotExists(containerName, { publicAccessLevel: 'blob' }, err => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ message: 'Container created' });
+            }
+        });
+    });
+};
+
+const upload = () => {
+    return new Promise((resolve, reject) => {
+        blobService.createBlockBlobFromLocalFile(containerName, blobName, sourceFilePath, err => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ message: 'Upload complete' });
+            }
+        });
+    });
+};
+
+const download = () => {
+    const dowloadFilePath = sourceFilePath;
+    return new Promise((resolve, reject) => {
+        blobService.getBlobToLocalFile(containerName, blobName, dowloadFilePath, err => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ message: 'Download complete' });
+            }
+        });
+    });
+};
+
+const list = () => {
+    return new Promise((resolve, reject) => {
+        blobService.listBlobsSegmented(containerName, null, (err, data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ message: `Items in container: ${containerName}`, data: data });
+            }
+        });
+    });
+};
+
+const deleteBlock = () => {
+    return new Promise((resolve, reject) => {
+        blobService.deleteBlobIfExists(containerName, blobName, err => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve({ message: 'Block blob deleted' });
+            }
+        });
+    });
+};
+
+const _module = {
+    "createContainer": createContainer,
+    "upload": upload,
+    "download": download,
+    "delete": deleteBlock,
+    "list": list
+};
+
+async function executeCommand() {
+    const response = await _module[args.command]();
+
+    console.log(response.message);
+
+    if(response.data) {
+        response.data.entries.forEach((entry) => {
+            console.log('Name:', entry.name, ' Type:', entry.blobType)
+        });
+    }
+};
+
+try {
+    console.log(`Executing '${args.command}'...`);
+    executeCommand();
+} catch(e) {
+    console.log(e);
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+
+
+# Storage Blobs Quickstart (rewrite)
+
+The following sample is a rewrite of the Azure [Storage Blobs Quickstart](https://github.com/Azure-Samples/storage-blobs-node-quickstart) by updating the sample in the following ways:
+
+- **Command-based interaction**: Rather than all operations executing sequentially, you are able to explicitly tell the script which operation to execute (ex: upload, list, etc.)
+
+- **Uses async/await**: The [Azure Storage SDK API](https://github.com/Azure/azure-storage-node) is still callback-based, but the approach in this sample modernizes the syntax. API calls are wrapped in `Promises` and executed in the context of an `async/await` operation.
+
+- **Based on a single file**: The original sample uses a technique to create unique file names which allows you to easily upload multiple files. The approach in this sample attempts to simplify the process and only allows you to manipulate one file by default.
+
+To run this sample you need an [Azure account](https://azure.microsoft.com/free/), a [blob storage account](https://docs.microsoft.com/azure/storage/common/storage-create-storage-account) and the associated blob storage connection string.
+
+## Set up
+First, clone the repository on your machine:
+
+    git clone 
+
+Next, install the dependencies:
+
+    npm install
+
+Now, update add your blob storage connection string to a file named `.env`.
+
+> **Note**: The repository includes a file named `.env.example`. You can rename this file by removing `.example` and adding the correct value for your connection string in the `.env` file.
+
+## Running the sample
+
+Once the setup steps are complete, you can interact with the sample by padding a command into the `--command` parameter.
+
+For instance if you want to upload the text file to Azure run the following command:
+
+    node index.js --command upload
+
+Commands available include:
+
+
+| Command | Description |
+|---------|---------|
+|`createContainer` | Creates a container named `test` (succeeds even if container already exists) |
+|`upload`          | Uploads the `example.txt` file |
+|`download`        | Downloads the contents of the `example` blob to `example.txt` |
+|`delete`          | Deletes the `example` blob |
+|`list`            | Lists the contents of the `test` container to the console |
